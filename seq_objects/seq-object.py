72795ae44d1e7e8fa941289d899af5d8f29de980
Sourav Singh <souravsingh@users.noreply.github.com>
2015-12-12 19:49:31 +0530

    Create seq-object.py

---
+++ seq_objects/seq-object.py
@@ -0,0 +1,7 @@
+from Bio.Seq import Seq
+from Bio.Alphabet import IUPAC
+my_seq = Seq("GATCG", IUPAC.unambiguous_dna) 
+for index, letter in enumerate(my_seq):
+  print("%i %s" % (index, letter))
+  
+  print(my_seq)
