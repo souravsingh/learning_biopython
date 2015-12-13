50cda6d80b183ee85c0f4063f839e19f3fd448c2
Sourav Singh <souravsingh@users.noreply.github.com>
2015-12-13 16:13:45 +0530

    Update convert_gb_to_fasta.py

--- write_seq/convert_gb_to_fasta.py
+++ write_seq/convert_gb_to_fasta.py
@@ -1,5 +1,5 @@
 from Bio import SeqIO
-input_filename = "NC_000913.gbk"
-output_filename = "NC_000913_converted.fasta"
+input_filename = "NC_03361.gbk"
+output_filename = "NC_013361_converted.fasta"
 count = SeqIO.convert(input_filename, "gb", output_filename, "fasta")
 print(str(count) + " records converted")
