f95cf89df4e5ed099cabe29f190979792f2d614c
Sourav Singh <souravsingh@users.noreply.github.com>
2015-12-14 17:26:44 +0530

    Update bases.py

--- using-seqfeatures/bases.py
+++ using-seqfeatures/bases.py
@@ -1,5 +1,5 @@
 from Bio import SeqIO
-record = SeqIO.read("NC_000913.gbk", "genbank")
+record = SeqIO.read("NC_013361.gbk", "genbank")
 bases = set() # Python's built in set datatype
 for feature in record.features:
     if feature.type == "gene":
